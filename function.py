3142471a0585a2de575f5abcc2b21102dd2b5018
Bishalpaudel999 <bishalpaudel710@gmail.com>
2025-12-22 21:03:43 +0545

    Add files via upload

---
+++ function.py
@@ -0,0 +1,39 @@
+# def generer():
+#     print('hello world from function.py')
+
+# # generer()
+
+# def detail(name,age):
+#     print(name,age)
+
+# # detail('bishal',23)
+# def multiply(x,y):
+#     return x * y
+# result = multiply(5,6)
+# print("the multiplication is ", result)
+
+# def info(name):
+#     print(f'hello, my name is {name}')
+    
+
+# info('bishal')
+# def total(* numbers):
+#    print(numbers)
+
+#    total(1,2,3,4)
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+x= 10
+def add():
+    print(x + 5)
+
+add()
+print(x)
+
---
+++ print.py
@@ -0,0 +1,44 @@
+ #print('1')
+# age = 34
+# name = 'hera'
+# #print(age, name)
+# #print(f'hello, my name is {name}, and I am {age} years old.')
+# x= 'we are barcelona'
+# y = x.split
+#print(y())
+#print(f'the best team in the world is {x},and the split of  {x} is {y()}')
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# x= "bishal"
+# y = "paudel"
+# z= "keep going"
+# print(x,y,z)
+# x= "bishal"
+# print(f'hello, my name is {x}')
+# def greet():
+#     print("Hello, World!")
+
+# print("this is the msg of  " ,greet())
+# def greet(name):
+#     print("Hello", name)
+
+# greet("Bishal")
+# def sum(x,y):
+#     return x+y
+# result = sum(5,6)
+# # print("the sum is ", result)
+# def factor(a, b):
+#     return a * b    
+
+# x = factor(5, 6)
+# print("the factor is ", x)
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
